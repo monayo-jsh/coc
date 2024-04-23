323ab239984b37826fdd31b22be4cffe41b59068
devch96 <chjung1006@gmail.com>
2024-04-23 18:22:46 +0900

    chore: RestClientConfig 패키지 이동

--- src/main/java/open/api/coc/external/coc/config/RestClientConfig.java
+++ src/main/java/open/api/coc/external/coc/config/RestClientConfig.java
@@ -1,7 +1,6 @@
-package open.api.coc.external.coc.clan.config;
+package open.api.coc.external.coc.config;
 
 import lombok.RequiredArgsConstructor;
-import open.api.coc.external.coc.config.ClashOfClanConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;

c233dbea58406602fb52f0d307c4024e06760117
주상현 <monayo.git@gmail.com>
2024-04-25 18:50:50 +0900

    Clash of Clan 서버 점검 시 메인 화면 서버 점검 알림 표시로 개선

--- src/main/java/open/api/coc/clans/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/open/api/coc/clans/common/exception/handler/GlobalExceptionHandler.java
@@ -4,9 +4,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(value = HttpServerErrorException.class)
+    public ResponseEntity<String> httpServerErrorException(HttpServerErrorException e) {
+        return ResponseEntity.status(e.getStatusCode())
+                             .body(e.getResponseBodyAsString());
+    }
 
     @ExceptionHandler(value = HttpClientErrorException.class)
     public ResponseEntity<String> httpClientErrorException(HttpClientErrorException e) {

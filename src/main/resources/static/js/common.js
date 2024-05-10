3db2063fe0f0206c5091b05b6a91860f259f6481
주상현 <monayo.git@gmail.com>
2024-05-10 13:07:52 +0900

    클랜 카드 표기 UI 수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -37,6 +37,47 @@ function settingTooltip(element, tooltip) {
   // <script src="https://unpkg.com/@popperjs/core@2"></script>
   // <script src="https://unpkg.com/tippy.js@6"></script>
   tippy(element, {
-    content: tooltip
+    content: tooltip,
+    animation: 'fade',
+    theme: 'darkgray',
+    arrow: false
   });
+}
+
+function convLeagueName(leagueName) {
+  switch (leagueName) {
+    case 'Unranked': return '랭크되지 않음';
+    case 'Bronze League III': return '브론즈 리그 III';
+    case 'Bronze League II': return '브론즈 리그 II';
+    case 'Bronze League I': return '브론즈 리그 I';
+    case 'Silver League III': return '실버 리그 III';
+    case 'Silver League II': return '실버 리그 II';
+    case 'Silver League I': return '실버 리그 I';
+    case 'Gold League III': return '골드 리그 III';
+    case 'Gold League II': return '골드 리그 II';
+    case 'Gold League I': return '골드 리그 I';
+    case 'Crystal League III': return '크리스탈 리그 III';
+    case 'Crystal League II': return '크리스탈 리그 II';
+    case 'Crystal League I': return '크리스탈 리그 I';
+    case 'Master League III': return '마스터 리그 III';
+    case 'Master League II': return '마스터 리그 II';
+    case 'Master League I': return '마스터 리그 I';
+    case 'Champion League III': return '챔피언 리그 III';
+    case 'Champion League II': return '챔피언 리그 II';
+    case 'Champion League I': return '챔피언 리그 I';
+    case 'Titan League III': return '타이탄 리그 III';
+    case 'Titan League II': return '타이탄 리그 II';
+    case 'Titan League I': return '타이탄 리그 I';
+    case 'Legend League': return '전설 리그';
+  }
+  return leagueName;
+}
+
+function convClanJoinTypeName(type) {
+  switch (type.toLowerCase()) {
+    case 'open': return '공개';
+    case 'inviteonly': return '초대 한정';
+    case 'closed': return '비공개'
+  }
+  return type;
 }

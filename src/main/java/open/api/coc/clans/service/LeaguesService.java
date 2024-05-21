cbd7977347000d128ea61d8d6f0b7508b4a71eea
주상현 <monayo.git@gmail.com>
2024-05-21 13:06:12 +0900

    리그 목록 조회 DB로 수정

--- src/main/java/open/api/coc/clans/service/LeaguesService.java
+++ src/main/java/open/api/coc/clans/service/LeaguesService.java
@@ -3,29 +3,24 @@ package open.api.coc.clans.service;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import open.api.coc.clans.common.ExceptionCode;
-import open.api.coc.clans.common.exception.CustomRuntimeException;
+import open.api.coc.clans.database.entity.league.LeagueEntity;
+import open.api.coc.clans.database.repository.common.LeagueRepository;
 import open.api.coc.clans.domain.clans.LabelResponse;
 import open.api.coc.clans.domain.clans.converter.LabelResponseConverter;
-import open.api.coc.external.coc.clan.ClanApiService;
-import open.api.coc.external.coc.clan.domain.leagues.LabelList;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LeaguesService {
 
-    private final ClanApiService clanApiService;
-
+    private final LeagueRepository leagueRepository;
     private final LabelResponseConverter labelResponseConverter;
 
     public List<LabelResponse> getLeagues() {
-        LabelList labelList = clanApiService.findLeagues()
-                                            .orElseThrow(() -> CustomRuntimeException.create(ExceptionCode.EXTERNAL_ERROR, "리그 목록 조회 실패"));
+        List<LeagueEntity> leagueEntities = leagueRepository.findAll();
 
-        return labelList.getItems()
-                        .stream()
-                        .map(labelResponseConverter::convert)
-                        .collect(Collectors.toList());
+        return leagueEntities.stream()
+                             .map(labelResponseConverter::convert)
+                             .collect(Collectors.toList());
     }
 }

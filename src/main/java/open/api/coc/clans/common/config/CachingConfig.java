c7397749c679ee803bb5b610abd31de223af87c7
주상현 <monayo.git@gmail.com>
2024-05-31 13:30:47 +0900

    cache config 비활성화 처리

--- src/main/java/open/api/coc/clans/common/config/CachingConfig.java
+++ src/main/java/open/api/coc/clans/common/config/CachingConfig.java
@@ -1,16 +1,13 @@
 package open.api.coc.clans.common.config;
 
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableCaching
+//@Configuration
+//@EnableCaching
 public class CachingConfig {
 
-    @Bean
+//    @Bean
     public CacheManager playerCacheManager() {
         ConcurrentMapCacheManager players = new ConcurrentMapCacheManager("players");
         players.setAllowNullValues(false);

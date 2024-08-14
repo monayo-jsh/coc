0c7afdded5eb4d4cbf569e618e6f3153ff56340b
주상현 <monayo.git@gmail.com>
2024-08-14 15:39:16 +0900

    Swagger 도입 및 API 명세 작성 진행

--- src/main/java/open/api/coc/clans/common/config/WebMvcConfig.java
+++ src/main/java/open/api/coc/clans/common/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggerInterceptor())
-                .excludePathPatterns("/static/**");
+                .excludePathPatterns("/static/**", "/clan/cms/docs/**");
     }
 
     @Override

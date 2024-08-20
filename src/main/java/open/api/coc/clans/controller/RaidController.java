94d1b6713a1293061a245158470290fed2ed8843
주상현 <monayo.git@gmail.com>
2024-08-20 10:15:24 +0900

    클랜 캐피탈 연동 규격 작성

--- src/main/java/open/api/coc/clans/controller/RaidController.java
+++ src/main/java/open/api/coc/clans/controller/RaidController.java
@@ -1,5 +1,11 @@
 package open.api.coc.clans.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import open.api.coc.clans.domain.raid.ClanCapitalRaidSeasonResponse;
@@ -7,8 +13,14 @@ import open.api.coc.clans.domain.raid.RaidScoreResponse;
 import open.api.coc.clans.domain.ranking.RankingHallOfFame;
 import open.api.coc.clans.service.RaidService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "클랜 캐피탈", description = "클랜 캐피탈 관련 기능")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/raid")
@@ -16,12 +28,28 @@ public class RaidController {
 
     private final RaidService raidService;
 
+    @Operation(
+        summary = "클랜 캐피탈 데이터 수집용 API, version: 1.00, Last Update: 24.08.20",
+        description = "클랜 캐피탈 데이터 수집용 API<br/>서버에 저장된 클랜 중 캐피탈 컨텐츠가 활성화된 클랜을 기준으로 데이터 수집"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공 응답 Body", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = Object.class)))
+    })
     @PostMapping("/collect/seasons")
     public ResponseEntity<String> collectClanCapitalRaidSeason() {
         raidService.collectClanCapitalRaidSeason();
         return ResponseEntity.ok().body("OK");
     }
 
+    @Operation(
+        summary = "클랜 캐피탈 조회 API (실시간 연동), version: 1.00, Last Update: 24.08.20",
+        description = "클랜 캐피탈 조회 API (실시간 연동)"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공 응답 Body", content = @Content(schema = @Schema(implementation = ClanCapitalRaidSeasonResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = Object.class)))
+    })
     @GetMapping("/{clanTag}/seasons")
     public ResponseEntity<ClanCapitalRaidSeasonResponse> getClanCapitalRaidSeasons(@PathVariable String clanTag) {
         ClanCapitalRaidSeasonResponse clanCapitalRaidAttacker = raidService.getClanCapitalRaidSeason(clanTag);
--- src/main/java/open/api/coc/clans/domain/raid/ClanCapitalRaidSeasonResponse.java
+++ src/main/java/open/api/coc/clans/domain/raid/ClanCapitalRaidSeasonResponse.java
@@ -1,5 +1,6 @@
 package open.api.coc.clans.domain.raid;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,13 +11,19 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ClanCapitalRaidSeasonResponse {
 
+    @Schema(description = "캐피탈 상태")
     private String state;
 
+    @Schema(description = "총 공격 가능 수")
     private final Integer totalAttackLimit = 50;
 
+    @Schema(description = "캐피탈 시작 시간")
     private Long startTime;
+
+    @Schema(description = "캐피탈 종료 시간")
     private Long endTime;
 
+    @Schema(description = "캐피탈 참여자 목록")
     private List<ClanCapitalRaidSeasonMemberResponse> members;
 
 }

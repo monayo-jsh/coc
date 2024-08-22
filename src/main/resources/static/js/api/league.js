439c78e20930d546ab76ed14fa00f313a5fdbb22
주상현 <monayo.git@gmail.com>
2024-08-22 09:30:29 +0900

    리그 관련 엔드포인트 조정

--- src/main/resources/static/js/api/league.js
+++ src/main/resources/static/js/api/league.js
@@ -1,5 +1,9 @@
+const PREFIX_LEAGUE_API = '/api/leagues' // 리그 API
+
+const URI_LEAGUES = `${PREFIX_LEAGUE_API}` // 리그 목록 조회
+
 async function fetchLeagues() {
-  return await axios.get("/leagues")
+  return await axios.get(URI_LEAGUES)
                     .then((response) => {
                       const { data } = response
                       return data;

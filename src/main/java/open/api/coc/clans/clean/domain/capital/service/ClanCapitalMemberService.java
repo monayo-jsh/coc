da0dfdfee35b123ad3a598ac662fe4a71706c401
주상현 <monayo.git@gmail.com>
2024-09-27 14:40:58 +0900

    클랜 캐피탈 도메인 서비스를 인터페이스와 구현체로 계층 분리 원복

--- src/main/java/open/api/coc/clans/clean/domain/capital/service/ClanCapitalMemberService.java
+++ src/main/java/open/api/coc/clans/clean/domain/capital/service/ClanCapitalMemberService.java
@@ -1,25 +1,77 @@
 package open.api.coc.clans.clean.domain.capital.service;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import open.api.coc.clans.clean.domain.capital.model.ClanCapitalRaidMember;
 import open.api.coc.clans.clean.domain.capital.model.ClanCapitalRaidMemberRankingDTO;
+import open.api.coc.clans.clean.domain.capital.repository.ClanCapitalRaidMemberRepository;
+import open.api.coc.clans.clean.infrastructure.capital.persistence.dto.RaiderRankingDTO;
+import open.api.coc.clans.clean.infrastructure.capital.persistence.entity.RaiderEntity;
+import open.api.coc.clans.clean.infrastructure.capital.persistence.mapper.ClanCapitalRaidMemberMapper;
+import open.api.coc.clans.common.config.HallOfFameConfig;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ClanCapitalMemberService {
+@Service
+@RequiredArgsConstructor
+public class ClanCapitalMemberService {
 
-    // 캐피탈 참여자 목록을 캐피탈 고유키로 조회한다.
-    List<ClanCapitalRaidMember> findByRaidId(Long raidId);
+    private final HallOfFameConfig hallOfFameConfig;
 
-    // 캐피탈 참여자 정보를 사용자 태그로 한다.
-    List<ClanCapitalRaidMember> findByTag(String playerTag, Integer limit);
+    private final ClanCapitalRaidMemberRepository clanCapitalRaidMemberRepository;
 
-    // 캐피탈 참여자 정보를 이름으로 조회한다.
-    List<ClanCapitalRaidMember> findByName(String playerName, Integer limit);
+    private final ClanCapitalRaidMemberMapper clanCapitalRaidMemberMapper;
 
-    // 캐피탈 획득 점수 랭킹을 시작일자로 조회한다.
-    List<ClanCapitalRaidMemberRankingDTO> rankingResourceLootedByStartDate(LocalDate startDate);
+    @Transactional(readOnly = true)
+    public List<ClanCapitalRaidMember> findByRaidId(Long raidId) {
+        List<RaiderEntity> raiderEntities = clanCapitalRaidMemberRepository.findAllByRaidId(raidId);
+        return mapToClanCapitalRaidMember(raiderEntities);
+    }
 
-    // 캐피탈 획득 점수 평균 랭킹을 시작일자 목록으로 조회한다.
-    List<ClanCapitalRaidMemberRankingDTO> rankingResourceLootedAverageByStartDates(List<LocalDate> startDates);
+    @Transactional(readOnly = true)
+    public List<ClanCapitalRaidMemberRankingDTO> rankingResourceLootedByStartDate(LocalDate startDate) {
+        Pageable pageable = Pageable.ofSize(hallOfFameConfig.getRanking());
+        List<RaiderRankingDTO> rankingDTOs = clanCapitalRaidMemberRepository.findAllResourceLootedRankingByStartDateAndPage(startDate, pageable);
+        return rankingDTOs.stream()
+                          .map(clanCapitalRaidMemberMapper::toClanCapitalRaidMemberRankingDTO)
+                          .toList();
+    }
 
+    @Transactional(readOnly = true)
+    public List<ClanCapitalRaidMemberRankingDTO> rankingResourceLootedAverageByStartDates(List<LocalDate> startDates) {
+        // 수집된 캐피탈 시즌이 비어있거나 하나인경우 지난주 랭킹 제공 불가
+        if (startDates.isEmpty() || startDates.size() == 1) {
+            return Collections.emptyList();
+        }
+
+        // 지난주 설정
+        List<LocalDate> lastWeeks = startDates.subList(1, startDates.size());
+
+        Pageable pageable = Pageable.ofSize(hallOfFameConfig.getRanking());
+        List<RaiderRankingDTO> rankingDTOs = clanCapitalRaidMemberRepository.findAllResourceLootedAverageRankingByStartDateAndPage(lastWeeks, pageable);
+        return rankingDTOs.stream()
+                          .map(clanCapitalRaidMemberMapper::toClanCapitalRaidMemberRankingDTO)
+                          .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<ClanCapitalRaidMember> findByTag(String playerTag, Integer limit) {
+        List<RaiderEntity> raiderEntities = clanCapitalRaidMemberRepository.findAllByPlayerTag(playerTag, limit);
+        return mapToClanCapitalRaidMember(raiderEntities);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ClanCapitalRaidMember> findByName(String playerName, Integer limit) {
+        List<RaiderEntity> raiderEntities = clanCapitalRaidMemberRepository.findAllByPlayerName(playerName, limit);
+        return mapToClanCapitalRaidMember(raiderEntities);
+    }
+
+    private List<ClanCapitalRaidMember> mapToClanCapitalRaidMember(List<RaiderEntity> raiderEntities) {
+        return raiderEntities.stream()
+                             .map(clanCapitalRaidMemberMapper::toClanCapitalRaidMember)
+                             .toList();
+    }
 }

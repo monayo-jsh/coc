4aa541f75047ceedca45bd42527b055c15f74b0c
주상현 <monayo.git@gmail.com>
2024-10-23 08:11:09 +0900

    클랜 게임 현황 응답 변수 조정

--- src/main/java/open/api/coc/clans/clean/presentation/clan/dto/ClanGameResponse.java
+++ src/main/java/open/api/coc/clans/clean/presentation/clan/dto/ClanGameResponse.java
@@ -1,13 +1,12 @@
 package open.api.coc.clans.clean.presentation.clan.dto;
 
 import java.util.List;
-import open.api.coc.clans.clean.domain.clan.model.ClanGameDTO;
 
 public record ClanGameResponse(
 
     String progressDate,
     Integer completedPoint,
-    List<ClanGameDTO> clanGames
+    List<ClanGameRecordResponse> records
 
 ) {
 }

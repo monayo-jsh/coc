6b41facf354cf3db289a3a65c5bbe942d42865e8
주상현 <monayo.git@gmail.com>
2024-11-22 15:44:12 +0900

    static prefix path 서버 config mapping 처리로 일괄 수정

--- src/main/java/open/api/coc/clans/common/config/WebMvcConfig.java
+++ src/main/java/open/api/coc/clans/common/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package open.api.coc.clans.common.config;
 
 import java.time.Duration;
+import java.util.List;
 import open.api.coc.clans.controller.interceptors.LoggerInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
@@ -11,16 +12,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private final List<String> resourcePaths = List.of("/favicon.ico", "/js/**", "/css/**", "/images/**", "/lib/**");
+    private final String swaggerDocPath = "/clan/cms/docs/**";
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggerInterceptor())
-                .excludePathPatterns("/static/**", "/clan/cms/docs/**");
+                .excludePathPatterns(resourcePaths)
+                .excludePathPatterns(swaggerDocPath);
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/images/**")
-                .addResourceLocations("classpath:/static/images/")
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/favicon.ico");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/lib/**").addResourceLocations("classpath:/static/lib/");
+
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/")
                 .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)).cachePublic());
     }
 }
